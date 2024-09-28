52a76c2f6b017c2574bc568a506d6d49fd5ea869
rockfactory <github@leonardfactory.com>
2024-09-29 01:54:35 +0200

    Add sentry vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: 'leonardfactory',
+      project: 'javascript-react',
+    }),
+  ],
+
   define: {
     APP_VERSION: JSON.stringify(require('./package.json').version),
     SENTRY_DSN: JSON.stringify(process.env.SENTRY_DSN),
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
