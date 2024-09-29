ab875c89a8472134a57c8feae94d4f0eaabd8dc3
rockfactory <github@leonardfactory.com>
2024-09-29 17:06:58 +0200

    fix: Updated somersloop image

--- src/factories/FactoryOutputRow.tsx
+++ src/factories/FactoryOutputRow.tsx
@@ -89,7 +89,7 @@ export function FactoryOutputRow(props: IFactoryOutputRowProps) {
           }}
           rightSection={
             <Image
-              src="/images/somersloop_256.png"
+              src="/images/somersloop_256_v2.png"
               alt="Somerloops"
               width={20}
               height={20}
