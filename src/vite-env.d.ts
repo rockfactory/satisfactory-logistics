5616c89a17c788261fae94fedb18f31f25a9e2ea
rockfactory <github@leonardfactory.com>
2024-10-30 03:27:33 +0100

    imp: Add preview banner

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="vite/client" />
 declare const APP_VERSION: string;
 declare const SENTRY_DSN: string;
+
+interface ImportMetaEnv {
+  readonly VITE_DEV_BANNER: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
