603a26c513e3e8eb494165e42d066c0507aa748e
rockfactory <github@leonardfactory.com>
2024-11-01 17:19:30 +0100

    imp: Some utils for measuring texts

---
+++ src/solver/layout/rendering/measureNode.ts
@@ -0,0 +1,54 @@
+import { getRecipeDisplayName } from '@/recipes/FactoryRecipe';
+import type { SolutionNode } from '@/solver/solveProduction';
+import { measureText } from './measureText';
+
+const BaseSizeWithoutTexts = {
+  Machine: {
+    width: 192.76,
+    height: 95.38,
+  },
+  Resource: {
+    width: 114.53,
+    height: 93.41,
+  },
+  Byproduct: {
+    width: 114.53,
+    height: 93.41,
+  },
+};
+
+/**
+ * Not working very well, but it's a start.
+ */
+export function measureNode(node: SolutionNode) {
+  switch (node.type) {
+    case 'Machine':
+      const firstLineText = getRecipeDisplayName(node.data.recipe);
+      const secondLineText = `x0.00 Machine`;
+      return {
+        width:
+          BaseSizeWithoutTexts.Machine.width +
+          Math.max(
+            measureText(firstLineText, 14),
+            measureText(secondLineText, 12),
+          ),
+        height: BaseSizeWithoutTexts.Machine.height,
+      };
+
+    case 'Resource':
+      return {
+        width:
+          BaseSizeWithoutTexts.Resource.width +
+          measureText(node.data.resource.displayName, 14),
+        height: BaseSizeWithoutTexts.Resource.height,
+      };
+
+    case 'Byproduct':
+      return {
+        width:
+          BaseSizeWithoutTexts.Byproduct.width +
+          measureText(node.data.resource.displayName, 14),
+        height: BaseSizeWithoutTexts.Byproduct.height,
+      };
+  }
+}
