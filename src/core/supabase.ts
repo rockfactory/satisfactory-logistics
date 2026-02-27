7a60e650dfd9a4c47e4acc703c805975f79a7ff0
Clark Tomlinson <fallen013@gmail.com>
2026-02-26 22:03:31 -0500

    replace supabase config

--- src/core/supabase.ts
+++ src/core/supabase.ts
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
-import { Database } from './database.types';
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "./database.types";
 
 export const supabaseClient = createClient<Database>(
-  'https://nymrtujjmzbhxcimjsci.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55bXJ0dWpqbXpiaHhjaW1qc2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc1MzY2ODcsImV4cCI6MjA0MzExMjY4N30.lOq-8ebIFLKQ1bmbw9bU4FQlKGWi-TNxyq6YVUTcR6w',
+	"https://tsiqtavbmponisvhfgxa.supabase.co",
+	"sb_publishable_UU5rxflsuZb78B9yEJwBvg_Y6p7SZC5",
 );
