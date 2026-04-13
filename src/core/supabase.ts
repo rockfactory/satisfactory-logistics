e9a0fd3000144d3e4da9de16ec635ec6f90346e9
rockfactory <github@leonardfactory.com>
2026-04-13 10:30:23 +0200

    deps: Swapped to new Supabase API keys

--- src/core/supabase.ts
+++ src/core/supabase.ts
@@ -3,5 +3,5 @@ import { Database } from './database.types';
 
 export const supabaseClient = createClient<Database>(
   'https://nymrtujjmzbhxcimjsci.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55bXJ0dWpqbXpiaHhjaW1qc2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc1MzY2ODcsImV4cCI6MjA0MzExMjY4N30.lOq-8ebIFLKQ1bmbw9bU4FQlKGWi-TNxyq6YVUTcR6w',
+  'sb_publishable_b2ytleL3Uz9w5NKkd1v7hg_FPiAl888',
 );
