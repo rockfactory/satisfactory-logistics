9231ed9433a4190befa2cc11e6d501d587c4252f
rockfactory <github@leonardfactory.com>
2026-04-18 01:53:28 +0200

    remove map header which took space

--- src/map/MapPage.tsx
+++ src/map/MapPage.tsx
@@ -1,7 +1,4 @@
-import { Group, Title } from '@mantine/core';
-import { IconMap2 } from '@tabler/icons-react';
 import { useStore } from '@/core/zustand';
-import { AfterHeaderSticky } from '@/layout/AfterHeaderSticky';
 import { FullHeightContainer } from '@/layout/FullHeightContainer';
 import { MapFiltersPanel } from './MapFiltersPanel';
 import classes from './MapPage.module.css';
@@ -11,23 +8,13 @@ export function MapPage() {
   const gameId = useStore(state => state.games.selected ?? null);
 
   return (
-    <>
-      <AfterHeaderSticky>
-        <Group gap="sm" justify="space-between">
-          <Group gap="sm">
-            <IconMap2 size={20} />
-            <Title order={4}>World Map</Title>
-          </Group>
-        </Group>
-      </AfterHeaderSticky>
-      <FullHeightContainer>
-        <div className={classes.layout}>
-          <MapFiltersPanel gameId={gameId} />
-          <div className={classes.mapArea}>
-            <WorldMapView gameId={gameId} />
-          </div>
+    <FullHeightContainer>
+      <div className={classes.layout}>
+        <MapFiltersPanel gameId={gameId} />
+        <div className={classes.mapArea}>
+          <WorldMapView gameId={gameId} />
         </div>
-      </FullHeightContainer>
-    </>
+      </div>
+    </FullHeightContainer>
   );
 }
