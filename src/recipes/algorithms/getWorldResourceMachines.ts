17d37f47b1804d4c14394b0296403c792c213518
rockfactory <github@leonardfactory.com>
2026-05-06 01:16:10 +0200

    fix: removed pressurizer

--- src/recipes/algorithms/getWorldResourceMachines.ts
+++ src/recipes/algorithms/getWorldResourceMachines.ts
@@ -3,7 +3,7 @@ import type { FactoryItem } from '@/recipes/FactoryItem';
 
 export function getWorldResourceMachines(resource: FactoryItem) {
   const machines = AllFactoryBuildings.filter(building => {
-    if (!building.extractor) return false;
+    if (!building.extractor?.itemsPerMinute) return false;
     if (
       building.extractor.allowedResources != null &&
       !building.extractor.allowedResources.includes(resource.id)
